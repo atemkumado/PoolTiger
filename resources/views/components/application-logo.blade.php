9763bebb30badf7004ff806b0e111295a8ae2ee0
Nguyễn Thành Long <73679885+atemkumado@users.noreply.github.com>
2024-01-23 13:54:24 +0700

    update

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,5 @@
 <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    @if (Route::currentRouteNamed('login'))
     <img src="{{ asset('images/TOG Talent Pool Logo.jpg') }}" alt="TOG Logo" style="width:300px; height: auto">
+   @endif
 </svg>
