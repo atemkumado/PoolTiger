45bfd58cadc11f4576df8b02d95f7d9bb5b60040
Nguyễn Thành Long <73679885+atemkumado@users.noreply.github.com>
2024-01-23 11:40:39 +0700

    update

---
+++ database/migrations/2024_01_19_032526_create_attachments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('talent_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('folderid')->nullable();
+            $table->string('smownerid')->nullable();
+            $table->string('modifiedtime')->nullable();
+            $table->string('filename')->nullable();
+            $table->string('path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attachments');
+    }
+};
